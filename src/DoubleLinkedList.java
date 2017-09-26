ba111e846328fd42f8292dae3159f41eec836be7
muukvds <muukvds@users.noreply.github.com>
2017-09-26 14:34:18 +0200

    add contains, remove and add method to DoubleLinkedList class

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -1,3 +1,50 @@
 public class DoubleLinkedList {
 
+   DoubleLinkedInt[] listStart = new DoubleLinkedInt[0];
+
+   public void add(int value)
+   {
+       DoubleLinkedInt[] tempList = new DoubleLinkedInt[listStart.length+1];
+
+       int i;
+
+       for (i =0; i < listStart.length;i++) {
+            tempList[i] = listStart[i];
+       }
+       i++;
+       tempList[i] = new DoubleLinkedInt(value);
+
+       listStart=tempList;
+   }
+
+   public void remove(int value)
+   {
+       DoubleLinkedInt[] tempList = new DoubleLinkedInt[listStart.length-1];
+
+        for(int i =0; i < listStart.length;i++)
+        {
+            if(listStart[i].getValue() != value){
+                tempList[i] = listStart[i];
+            }
+        }
+
+        listStart = tempList;
+
+   }
+
+   public boolean contains(int value)
+   {
+       boolean contains = false;
+
+       for(int i =0; i < listStart.length;i++)
+       {
+           if(listStart[i].getValue() == value){
+               contains = true;
+               break;
+           }
+       }
+
+       return contains;
+   }
+
 }
